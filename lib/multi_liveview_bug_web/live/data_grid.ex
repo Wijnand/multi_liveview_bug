cf8c80b134d8112d1496d52674847700fba4303a
Wijnand Wiersma <wijnand.wiersma@tellcharlie.com>
2019-10-07 11:09:28 +0200

    display currently selected filter

--- lib/multi_liveview_bug_web/live/data_grid.ex
+++ lib/multi_liveview_bug_web/live/data_grid.ex
@@ -1,12 +1,12 @@
 defmodule MultiLiveviewBugWeb.DataGridLive do
   use Phoenix.LiveView
-  import Calendar.Strftime
   alias MultiLiveviewBugWeb.Router.Helpers, as: Routes
 
   def render(assigns) do
     ~L"""
     <div>
       <h2>This is a fake data grid, rendered as a main page. It uses live_links for internal navigation within this view.</h2>
+      <p>Selected filter: <%= @filter %></p>
       <ul>
         <li>Navigation links</li>
         <li><%= live_link "Filter A", to: Routes.live_path(@socket, __MODULE__, filter: "filter_a") %></li>
